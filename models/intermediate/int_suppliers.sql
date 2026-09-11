421375c6f41068a9e84ccf062ecc03f104e08ccb
Murali Kannan <muralikannan25@gmail.com>
2026-09-11 06:27:48 +0000

    commit for job

--- models/intermediate/int_suppliers.sql
+++ models/intermediate/int_suppliers.sql
@@ -5,7 +5,7 @@ with supplier as (
 
     select supplier_id, n.sname supplier_name, s.* exclude (supplier_id, supplier_name) 
     from {{ ref('stg_suppliers') }} s
-    join dbt_murali.supplier_names n on s.supplier_id = n.skey 
+    join {{ ref('supplier_names') }} n on s.supplier_id = n.skey 
     
     {% if is_incremental() %}
         where updated_time > (select max(updated_time) from {{ this }})
--- tests/min_order_value.sql
+++ tests/min_order_value.sql
@@ -1,4 +1,4 @@
 select order_id, total_price ordertotal 
-from {{ ref('stg_orders') }}  where (ordertotal<=880)
+from {{ ref('stg_orders') }}  where (ordertotal<={{ var('v_min_order') }})
 
 
